5f5f4ff44953b41ab434faaf22e692fd71b8ff99
Prince Kushwaha <Prince Kushwaha : "kushwahaprince43668@gmail.com>
2026-06-01 23:10:12 +0530

    feat : add api calls to localhost

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const express = require('express');
 
 const app = express();
+app.get('/',(req,res)=>{
+    res.send('Hello world')
+})
+app.get('/about',(req,res)=>{
+    res.send('About page')
+})
 app.listen(3000);
 //update
