696452b6355d2bd1ed37688b64c95f60e969dfdd
Prince Kushwaha <kushwahaprince43668@gmail.com>
2026-06-04 22:57:55 +0530

    feat : implement PATCH method for updating data

--- src/app.js
+++ src/app.js
@@ -11,17 +11,20 @@ const notes=[]
 
 app.use(express.json()) //middleware to parse json data from request body
 app.post('/notes',(req,res)=>{
-    console.log(req.body);
-    notes.push(req.body);
+    const note = req.body;
+    notes.push(note);
     res.status(201).json({
         message:'Note created successfully',
+        notes:note
     });
     
 })
 
 
 app.get('/notes',(req,res)=>{
+    console.log(req.body);
     res.status(200).json({
+        
         message: "notes recived",
         notes:notes
     });
@@ -37,4 +40,14 @@ app.delete("/notes/:index",(req, res)=>{
     });
     
 })
+
+app.patch("/notes/:index",(req, res)=>{
+    const index = req.params.index;
+    const description = req.body.description;
+    notes[index].description = description;
+    res.status(200).json({
+        message: 'Note updated successfully',
+    })
+})
+
 module.exports = app;
