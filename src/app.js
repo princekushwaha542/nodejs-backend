9aedf896b544711875e403c63364bed6c0114164
Prince Kushwaha <kushwahaprince43668@gmail.com>
2026-06-06 22:18:58 +0530

    feat: add POST API to store notes in database

--- src/app.js
+++ src/app.js
@@ -1,54 +1,52 @@
 const express = require('express');
-
+const noteModel = require('./models/note.model');
 const app = express(); //create a server using express
-// app.get('/',(req,res)=>{
-//     res.send('Hello world')
-// })
-// app.get('/about',(req,res)=>{
-//     res.send('About page')
-// })
-const notes=[]
 
-app.use(express.json()) //middleware to parse json data from request body
-app.post('/notes',(req,res)=>{
-    const note = req.body;
-    notes.push(note);
-    res.status(201).json({
-        message:'Note created successfully',
-        notes:note
-    });
+app.use(express.json())//middleware to parse json data from request body
+
+
+app.post('/notes', async (req, res) => {
+    const data = req.body;
+    console.log(data)
+   await noteModel.create({
+    title : data.title,
+    description : data.description
+   })
+   res.status(201).json({
+    message: 'Note created successfully'
+   })
     
 })
 
 
-app.get('/notes',(req,res)=>{
-    console.log(req.body);
-    res.status(200).json({
+// app.get('/notes',(req,res)=>{
+//     console.log(req.body);
+//     res.status(200).json({
         
-        message: "notes recived",
-        notes:notes
-    });
+//         message: "notes recived",
+//         notes:notes
+//     });
     
-})
+// })
 
 
-app.delete("/notes/:index",(req, res)=>{
-    const index = req.params.index;
-    delete notes[index];
-    res.status(200).json({
-        message: 'Note deleted seccessfully',
-    });
+// app.delete("/notes/:index",(req, res)=>{
+//     const index = req.params.index;
+//     delete notes[index];
+//     res.status(200).json({
+//         message: 'Note deleted seccessfully',
+//     });
     
-})
+// })
 
-app.patch("/notes/:index",(req, res)=>{
-    const index = req.params.index;
-    const description = req.body.description;
-    notes[index].description = description;
-    res.status(200).json({
-        message: 'Note updated successfully',
-    })
-})
+// app.patch("/notes/:index",(req, res)=>{
+//     const index = req.params.index;
+//     const description = req.body.description;
+//     notes[index].description = description;
+//     res.status(200).json({
+//         message: 'Note updated successfully',
+//     })
+// })
 
 
 module.exports = app;
