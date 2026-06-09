baf02d59ac24484b9b6e21560b95f8881be1826c
Prince Kushwaha <kushwahaprince43668@gmail.com>
2026-06-09 23:57:36 +0530

    fix : fix post api

--- project1/src/app.js
+++ project1/src/app.js
@@ -1,8 +1,8 @@
 const express = require('express')
-const multer = require('multer')
+const multer = require('multer') // install multer 
 
 const app = express();
-app.use(express.json());
+app.use(express.json());// middleware to parse json data
 const upload = multer({storage : multer.memoryStorage()})
 
 app.post("/create-post", upload.single('image'),async (req,res) =>{
