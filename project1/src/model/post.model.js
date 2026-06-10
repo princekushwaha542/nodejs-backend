39ced21dcd682c28a049110651d3ffacd5ddc598
Prince Kushwaha <kushwahaprince43668@gmail.com>
2026-06-10 23:37:58 +0530

    fix : post model

--- project1/src/model/post.model.js
+++ project1/src/model/post.model.js
@@ -1,10 +1,18 @@
 const mongoose = require('mongoose');
 
 const postSchema = new mongoose.Schema({
-    caption : String,
-    image :  String,
-})
+    caption: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+});
 
-const postModel = mongoose.model('post', postSchema )
+const postModel = mongoose.model('Post', postSchema);
 
 module.exports = postModel;
