3cbd79a497aabec561417cb41fd6c45fe0347900
Prince Kushwaha <kushwahaprince43668@gmail.com>
2026-06-12 23:33:34 +0530

    feat : fix storage service

--- project1/service/storage.service.js
+++ project1/service/storage.service.js
@@ -2,4 +2,18 @@ const ImageKit = require('@imagekit/nodejs');
 
 const imagekit = new ImageKit({
     publicKey : process.env.IMAGEKIT_PUBLIC_KEY,
+
 })  
+
+module.exports = {
+    uploadImage: async (file) => {
+        try {
+            const response = await imagekit.upload({
+                file: file.buffer,
+                fileName: file.originalname
+            });
+            return response.url;
+        } catch (error) {
+            console.error('Error uploading image:', error);
+            throw new Error('Failed to upload image');
+        }   
