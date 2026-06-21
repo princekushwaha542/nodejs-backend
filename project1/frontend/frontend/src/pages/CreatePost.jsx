e7dde7144377d640dbe2b9a8b9155d506ec93f78
Prince Kushwaha <kushwahaprince43668@gmail.com>
2026-06-21 22:08:26 +0530

    feat(frontend): add create post form and input fields

---
+++ project1/frontend/frontend/src/pages/CreatePost.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const CreatePost = () => {
+  return (
+    <section className='create-post-section'>
+      <h1>Create Post</h1>
+        <form>
+            <input type="file" placeholder='upload image' name='image' accept='image/*' />
+            <input type="text" placeholder='caption' required name='caption' />
+            <button type='submit'>Submit</button>
+        </form>
+ 
+    </section>
+  )
+}
+
+export default CreatePost
