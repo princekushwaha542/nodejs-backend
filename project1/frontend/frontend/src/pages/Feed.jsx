d1f1175b42f0ec8cb01e5f2f7b25839b64112838
Prince Kushwaha <kushwahaprince43668@gmail.com>
2026-06-30 22:02:02 +0530

    fix: correct axios import and clean up Feed component

--- project1/frontend/frontend/src/pages/Feed.jsx
+++ project1/frontend/frontend/src/pages/Feed.jsx
@@ -1,37 +1,39 @@
-import React, { useState , useEffect } from 'react'
-import axios from axios 
-import { useActionState } from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 const Feed = () => {
-const [posts , setpost] = useState([
-  {
-    _id : "1",
-    caption: "test_caption",
-    image: "https://ik.imagekit.io/gwffhfxyd/image_ZAgvVmc4z.jpg",
-  
-  }
-])
-useEffect(()=>{
-  axios.get("http://localhost:3000/create-post")
-  .then((res)=>{
-    console.log(res.data);
-  })
-})
+  const [posts, setpost] = useState([
+    {
+      _id: "1",
+      caption: "test_caption",
+      image: "https://ik.imagekit.io/gwffhfxyd/image_ZAgvVmc4z.jpg",
+    }
+  ]);
 
+  useEffect(() => {
+    axios.get("http://localhost:3000/create-post")
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
- return (
-  <section className='feed-section'>
-    {posts.length > 0 ? (
-      posts.map((post) => (
-        <div key={post._id} className='post-card'>
-        <img src={post.image} alt={post.caption} />
-        <p>{post.caption}</p>
-        </div>
-      ))
-    ) : (
-      <p>No posts found</p>
-    )}
-  </section>
-);
+  return (
+    <section className='feed-section'>
+      {posts.length > 0 ? (
+        posts.map((post) => (
+          <div key={post._id} className='post-card'>
+            <img src={post.image} alt={post.caption} />
+            <p>{post.caption}</p>
+          </div>
+        ))
+      ) : (
+        <p>No posts found</p>
+      )}
+    </section>
+  );
+};
 
-}
-export default Feed
+export default Feed;
