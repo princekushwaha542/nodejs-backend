32e8291282b6d0d169a40616dcedab2a9dcb2f79
Prince Kushwaha <kushwahaprince43668@gmail.com>
2026-07-02 21:56:55 +0530

    feat: connect frontend form with create post API

--- project1/frontend/frontend/src/pages/CreatePost.jsx
+++ project1/frontend/frontend/src/pages/CreatePost.jsx
@@ -4,11 +4,15 @@ const CreatePost = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     const formData = new FormData(e.target);
+    axios.post('http://localhost:3000/create-post', formData)
+     .then((res => {
+      console.log(res);
+     }))
   }
   return (
     <section className='create-post-section'>
       <h1>Create Post</h1>
-        <form>
+        <form onSubmit={handleSubmit}>
             <input type="file" placeholder='upload image' name='image' accept='image/*' />
             <input type="text" placeholder='Enter Caption' required name='caption' />
             <button type='submit'>Submit</button>
