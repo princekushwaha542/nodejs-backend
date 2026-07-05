a33a527593cf6d8e5388e46a19cf4a4dffdfae4b
Prince Kushwaha <kushwahaprince43668@gmail.com>
2026-07-05 22:00:51 +0530

    feat: connect MongoDB using environment variables

--- auth-system/src/db/db.js
+++ auth-system/src/db/db.js
@@ -5,7 +5,7 @@
     async function connectDB(){
 
         try{
-            await mongoose.connect(process.env.MONGO_URL)
+            await mongoose.connect(process.env.MONGO_URI)
             console.log('Database connected successfully')
         } catch (error) {
             console.error('Error connecting to database:', error)
