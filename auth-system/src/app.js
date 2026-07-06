8674dbbe932840f8a70dc35c4d9d52d4c4b97cfc
Prince Kushwaha <kushwahaprince43668@gmail.com>
2026-07-06 22:20:36 +0530

    feat : connect register controller to auth route

--- auth-system/src/app.js
+++ auth-system/src/app.js
@@ -1,9 +1,10 @@
-const express = require('express')
-
+const express = require('express');
+const authRoutes = require('../src/routes/auth.routes');
 
 
 const app = express();
-
+app.use(express.json());
+app.use('/auth', authRoutes);
 
 
 
