22eb6c1996601d33ef8fb651d0be9eb09e74e1f4
Prince Kushwaha <kushwahaprince43668@gmail.com>
2026-07-09 22:48:35 +0530

    feat : add cookie-parser middleware support

--- auth-system/src/app.js
+++ auth-system/src/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const authRoutes = require('../src/routes/auth.routes');
+const cookieParser = require('cookie-parser');
+
 
 
 const app = express();
 app.use(express.json());
 app.use('/auth', authRoutes);
-
+app.use(cookieParser());
 
 
 module.exports = app;
