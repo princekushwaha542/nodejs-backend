169b7ca4d4302627bcc853a420378ba1aaa095c4
Prince Kushwaha <kushwahaprince43668@gmail.com>
2026-07-11 23:36:19 +0530

    Add :  test route JSON response

--- auth-system/src/routes/auth.routes.js
+++ auth-system/src/routes/auth.routes.js
@@ -3,6 +3,14 @@ const authcontroller = require("../controllers/auth.controllers");
 
 const router = express.Router();
 
-router.post('/register', authcontroller.registerUser);
+router.post('/register', authcontroller.registerUser){
+    router.gest('/test', (req, res) => {
+        console.log("cocing test route");
+    })
+    res.status(200).json({
+        message: "test route working"
+    })  
+}
+
 
 module.exports = router;
