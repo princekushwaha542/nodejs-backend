11d73faa3a7b044894e1ad8c34daaca74a5348b1
Prince Kushwaha <kushwahaprince43668@gmail.com>
2026-07-12 22:17:16 +0530

    feat: enforce unique email in user schema

--- auth-system/src/models/user.model.js
+++ auth-system/src/models/user.model.js
@@ -2,11 +2,15 @@ const mongoose = require('mongoose');
 
 
 const userSchema = new mongoose.Schema({
-    username:'string',
-    email:'string',
-    password:'string'
+    username:String,
+    email:{
+        type:String,
+        unique:true
+    },
+    password:String
     
 })
+
 
 const userModel = mongoose.model('user',userSchema);
 
--- auth-system/src/routes/auth.routes.js
+++ auth-system/src/routes/auth.routes.js
@@ -3,14 +3,15 @@ const authcontroller = require("../controllers/auth.controllers");
 
 const router = express.Router();
 
-router.post('/register', authcontroller.registerUser){
-    router.gest('/test', (req, res) => {
-        console.log("cocing test route");
-    })
+router.post('/register', authcontroller.registerUser);
+
+router.get('/test', (req, res) => {
+    console.log("cookies", req.cookies);
     res.status(200).json({
-        message: "test route working"
+        message: "test route working",
+        cookies : req.cookies
     })  
-}
+})
 
 
 module.exports = router;
